17251ab82552e3e802207c958f37eed55ae99b41
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-08-03 18:43:02 +0200

    Change database

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -77,10 +77,8 @@ WSGI_APPLICATION = 'polls_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'polls',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
