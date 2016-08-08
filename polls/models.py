7efac5855b459a33f428c29081b3834075eb7411
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-08-08 13:22:13 +0200

    Add type Choice

--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,21 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     end_date = models.DateTimeField()
+
+    SINGLE = 'S'
+    MULTIPLE = 'M'
+    OPEN = 'O'
+    TYPE_CHOICES = (
+        ('SINGLE', 'Single'),
+        ('MULTIPLE', 'Multiple'),
+        ('OPEN', 'Open'),
+    )
+    choice_type = models.CharField(
+        max_length=8,
+        choices=TYPE_CHOICES,
+        default=SINGLE,
+    )
+
     def __str__(self):
         return self.question_text
 
@@ -16,5 +31,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
     def __str__(self):
         return self.choice_text
