ba6bb1717482ad930707756ff8d64fcdb47e1a21
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-08-08 18:09:41 +0200

    Add LOGIN_REDIRECT_URL

--- polls_project/settings.py
+++ polls_project/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 
 # Django registration redux
 REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
+LOGIN_REDIRECT_URL = '/polls/'  # The page you want users to arrive at after they successful log in
+LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+                                                                # and are trying to access pages requiring authentication
