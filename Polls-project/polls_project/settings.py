01a2593188dc84cf5e76acedc37163d2a7000403
Nicola Dilillo <nicoladilillo98@gmail.com>
2016-09-19 19:38:23 +0200

    Fix a error

--- Polls-project/polls_project/settings.py
+++ Polls-project/polls_project/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+MEDIA_ROOT = 'media'
+STATIC_URL = 'static'
 
 # Django registration redux
 REGISTRATION_OPEN = True
